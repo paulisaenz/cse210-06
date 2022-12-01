c3c23656b5f5b726be50da99dda1d130b2cd109e
Alison Mazzarella-Woelzl <104347449+AlisonMazzarella@users.noreply.github.com>
2022-12-01 13:12:58 -0500

    Cleaned up comments

--- game/casting/pacman.py
+++ game/casting/pacman.py
@@ -4,10 +4,10 @@ from game.casting.point import Point
 
 
 class Pacman(Actor):
-    """The main character of the game"""
+    """The main character of the game."""
     
     def __init__(self, body, animation, debug = False):
-        """Constructs a new character.
+        """Constructs a new PacMan.
         
         Args:
             body: A new instance of Body.
@@ -19,7 +19,7 @@ class Pacman(Actor):
         self._animation = animation
 
     def get_animation(self):
-        """Gets the bat's animation.
+        """Gets PacMan's animation.
         
         Returns:
             An instance of Animation.
@@ -27,7 +27,7 @@ class Pacman(Actor):
         return self._animation
 
     def get_body(self):
-        """Gets the bat's body.
+        """Gets PacMan's body.
         
         Returns:
             An instance of Body.
@@ -35,34 +35,33 @@ class Pacman(Actor):
         return self._body
 
     def move_next(self):
-        """Moves the character using its velocity."""
+        """Moves PacMan using its velocity."""
         position = self._body.get_position()
         velocity = self._body.get_velocity()
         new_position = position.add(velocity)
         self._body.set_position(new_position)
 
     def swing_left(self):
-        """Steers the character to the left."""
+        """Steers PacMan to the left."""
         velocity = Point(-RACKET_VELOCITY, 0)
         self._body.set_velocity(velocity)
         
     def swing_right(self):
-        """Steers the character to the right."""
+        """Steers PacMan to the right."""
         velocity = Point(RACKET_VELOCITY, 0)
         self._body.set_velocity(velocity)
 
-    #I don't really know how to manage this - P
     def swing_up(self):
-        """Steers the character up."""
+        """Steers PacMan up."""
         velocity = Point(RACKET_VELOCITY, 0)
         self._body.set_velocity(velocity)
 
     def swing_down(self):
-        """Steers the character down."""
+        """Steers PacMan down."""
         velocity = Point(RACKET_VELOCITY, 0)
         self._body.set_velocity(velocity)
     
     def stop_moving(self):
-        """Stops the bat from moving."""
+        """Stops PacMan from moving."""
         velocity = Point(0, 0)
         self._body.set_velocity(velocity)
