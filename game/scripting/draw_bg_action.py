a69926160bf26a7feb38cafd946fa3af3b58b6c6
DiabGarren <garrendiab@gmail.com>
2022-12-06 11:54:05 +0200

    Added draw background action

---
+++ game/scripting/draw_bg_action.py
@@ -0,0 +1,20 @@
+from constants import *
+from game.scripting.action import Action
+
+
+class DrawBGAction(Action):
+
+    def __init__(self, video_service):
+        self._video_service = video_service
+        
+    def execute(self, cast, script, callback):
+        bg = cast.get_first_actor(BG_GROUP)
+        body = bg.get_body()
+
+        if bg.is_debug():
+            rectangle = body.get_rectangle()
+            self._video_service.draw_rectangle(rectangle, PURPLE)
+            
+        image = bg.get_image()
+        position = body.get_position()
+        self._video_service.draw_image(image, position)
