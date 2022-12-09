d77b463d4f1369bf02b3fcd180cb4712f437873a
Alasa <ogboanohrichardjamb2020@gmail.com>
2022-12-09 19:07:17 +0100

    Nice everything works

--- game/casting/brick.py
+++ game/casting/brick.py
@@ -4,9 +4,9 @@ from game.casting.actor import Actor
 class Brick(Actor):
     """A solid, rectangular object that can be broken."""
 
-    def __init__(self, body, image, points, debug = False):
+    def __init__(self, body, image, points, debug=False):
         """Constructs a new Brick.
-        
+
         Args:
             body: A new instance of Body.
             image: A new instance of Image.
@@ -16,10 +16,10 @@ class Brick(Actor):
         self._body = body
         self._image = image
         self._points = points
-        
+
     def get_image(self):
         """Gets the ball's image.
-        
+
         Returns:
             An instance of Image.
         """
@@ -27,7 +27,7 @@ class Brick(Actor):
 
     def get_body(self):
         """Gets the brick's body.
-        
+
         Returns:
             An instance of Body.
         """
@@ -35,7 +35,7 @@ class Brick(Actor):
 
     def get_points(self):
         """Gets the brick's points.
-        
+
         Returns:
             A number representing the brick's points.
         """
